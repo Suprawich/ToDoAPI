935306fc6a636b807e42c4a3863d398f52ca9511
AkarimaP <103369577+AkarimaP@users.noreply.github.com>
2023-10-28 23:51:26 +0700

    apikung

--- Controllers/ActivitiesController.cs
+++ Controllers/ActivitiesController.cs
@@ -24,12 +24,14 @@ public class ActivitiesController : ControllerBase
     {
         
         var db = new ToDoDbContext();
-        var activity = from a in db.Activity /*where a.Id == Id*/ select a;
-        // new {
-        //     id = a.Id,
-        //     name = a.Name,
-        //     when = a.When
-        // }).FirstOrDefault();
+        var UserId= User.Identity.Name;
+
+        var activity = from a in db.Activity where a.UserId == UserId select 
+        new {
+            id = a.Id,
+            name = a.Name,
+            when = a.When
+        };   //.FirstOrDefault();
 
         if (activity == null) return NotFound();
         return Ok(activity);
@@ -44,8 +46,12 @@ public class ActivitiesController : ControllerBase
         var a = new Models.Activity(); //new record
         a.Name = data.Name;
         a.When = data.When;
+        a.UserId = User.Identity.Name;
         db.Activity.Add(a);
         db.SaveChanges();
+
+        // Reload the entity from the database to get the generated Id
+        db.Entry(a).GetDatabaseValues();
 
         return Ok(new {id = a.Id}); //sent Id to frontend
     }
@@ -56,12 +62,19 @@ public class ActivitiesController : ControllerBase
     public IActionResult Put(uint Id, [FromBody] DTOs.Activity data)
     {
         var db = new ToDoDbContext();
-        
-        var a = db.Activity.Find(Id);
-        if (a == null) return NotFound();
+        var UserId= User.Identity.Name;
 
-        a.Name = data.Name;
-        a.When = data.When;
+        var activities = from a in db.Activity where a.UserId == UserId & a.Id==Id select new {
+                User_id=a.UserId,
+            };
+
+        if(!activities.Any()) return NoContent();
+        
+        var b = db.Activity.Find(Id);
+        if (b == null) return NotFound();
+
+        b.Name = data.Name;
+        b.When = data.When;
         db.SaveChanges();
 
         return Ok();
@@ -75,7 +88,7 @@ public class ActivitiesController : ControllerBase
         var db = new ToDoDbContext();
         
         var a = db.Activity.Find(Id);
-        if (a == null) return NotFound();
+        if (a == null | a.UserId != User.Identity.Name) return NotFound();
 
         db.Activity.Remove(a);
         db.SaveChanges();
